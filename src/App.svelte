2fd3776849f0a1d3e72d83fc0971b969e146bab0
IliasJl1 <simple.gratuit@gmail.com>
2022-04-07 16:56:25 +0000

    FE Adjustments

--- src/App.svelte
+++ src/App.svelte
@@ -16,12 +16,12 @@
 
   /***********************************/
   // Customise the app by changing the following variables.
-  const TITLE = "Shapes";
-  const DESCRTIPTION = "A collection of shapes on the blockchain";
-  const HEADER_TITLE = "shapes.xyz";
-  const HEADER_LINK = "https://solarare.com";
+  const TITLE = "Chimpixies";
+  const DESCRTIPTION = "The 333 Chimpixies are the  next generation of DAO.";
+  const HEADER_TITLE = "chimpixie.com";
+  const HEADER_LINK = "Chimpixies.com";
   // Your image or GIF needs to be in the /public folder for this to work
-  const IMAGE_LINK = "/example.gif";
+  const IMAGE_LINK = "/ChimpixiesLogo.gif";
   /***********************************/
 
   let { solana } = window as any;
